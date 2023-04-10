61fd815cbf40285bbafa31ccccebe6b1dff8b94d
Fumino <richberchannel01@gmail.com>
2023-04-11 01:57:29 +0700

    +add load events log

--- handlers/events.js
+++ handlers/events.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-
+const events = [];
+let dateNow = Date.now();
 
 module.exports = (cometta) => {
     const eventsPath = path.join(__dirname, '../events');
     const eventsFile = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
+    console.log("[x] :: ".magenta + "Now starting loading events...".brightYellow);
     for (const file of eventsFile) {
         const filePath = path.join(eventsPath, file);
         const event = require(filePath);
+        events.push(event);
         if (event.once) {
             cometta.once(event.name, (...args) => event.execute(...args));
         } else {
             cometta.on(event.name, (...args) => event.execute(...args));
         }
     }
+    console.log("[x] :: ".magenta + `Loaded ${events.length} events after: `.brightGreen + `${Date.now() - dateNow}ms`.green);
+    console.log('\n');
 }
