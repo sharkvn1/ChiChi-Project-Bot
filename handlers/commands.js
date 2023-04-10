1a0d0aaf48ee1717cf782638baa66f353c7e9386
Fumino <richberchannel01@gmail.com>
2023-04-11 01:56:35 +0700

    +add loading commands log +remove deploy slash commands

--- handlers/commands.js
+++ handlers/commands.js
@@ -1,35 +1,23 @@
-const { Collection, REST, Routes } = require('discord.js');
-const config = require('../Config/config.json');
 const fs = require('fs');
 const path = require('path');
 const cmds = [];
+let dateNow = Date.now();
 
 module.exports = (cometta) => {
-    cometta.commands = new Collection;
+    console.log("[x] :: ".magenta + `Now starting loading commands...`.brightYellow);
 
     const cmdPath = path.join(__dirname, '../commands');
     const cmdFiles = fs.readdirSync(cmdPath).filter(file => file.endsWith('.js'));
-    for (const file of cmdFiles){
+    for (const file of cmdFiles) {
         const filePath = path.join(cmdPath, file);
         const cmd = require(filePath);
-        cmds.push(cmd.data.toJSON());
-        if('data' in cmd && 'execute' in cmd){
+        cmds.push(cmd.data.name);
+        if ('data' in cmd && 'execute' in cmd) {
             cometta.commands.set(cmd.data.name, cmd);
         } else {
-            console.log(`[Warning] the cmd at ${filePath} is missing a required "data" or "execute" property`);
+            console.log(`[Warning] the cmd at ${filePath} is missing a required "data" or "execute" property`.brightRed);
         }
     }
-
-    const rest = new REST().setToken(config.token);
-
-    (async () => {
-        try {
-            const data = await rest.put(
-                Routes.applicationGuildCommands(config['cometta-Bot-Id'], config['cometta-Server-id']),
-                {body: cmds},  
-            );
-        } catch (error) {
-            console.log(error);
-        }
-    })
+    console.log("[x] :: ".magenta + `Loaded ${cmds.length} commands after: `.brightGreen + `${Date.now() - dateNow}ms`.green);
+    console.log('\n');
 }
