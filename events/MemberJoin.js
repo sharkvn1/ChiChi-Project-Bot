dfe11b3685ebfbb5ffaaeaa211c92af5ee08a1b0
Fumino <richberchannel01@gmail.com>
2023-04-19 22:01:22 +0700

    - Change the message when new user join

--- events/MemberJoin.js
+++ events/MemberJoin.js
@@ -1,17 +1,61 @@
-const {Events, EmbedBuilder } = require('discord.js');
+
+const { Events, EmbedBuilder, Client, AttachmentBuilder } = require('discord.js');
+const { createCanvas, Image } = require('@napi-rs/canvas');
+const { readFile } = require('fs/promises');
+const { request } = require('undici');
+const applyText = (canvas, text) => {
+    const context = canvas.getContext('2d');
+    let fontSize = 70;
+    do {
+        context.font = `${fontSize -= 10}px sans-serif`;
+    } while (context.measureText(text).width > canvas.width - 300);
+    return context.font;
+};
+
 module.exports = {
     name: Events.GuildMemberAdd,
 
     /**
      * 
-     * @param {interaction} interaction 
+     * @param {Client} cometta
      */
-    async execute(cometta){
-        const welcomeMessage = new EmbedBuilder()
-        .setTitle(`Welcome ${cometta.user.username} to out cometta discord server`)
-        .setDescription('Remember to read our rule and answer the question in the server guide');
+    async execute(cometta) {
+        const canvas = createCanvas(700, 250);
+        const context = canvas.getContext('2d');
 
-        cometta.user.send({ embeds: [welcomeMessage]});
-        
+        const bg = await readFile('./img/canvas.jpg');
+        const bgi = new Image();
+        bgi.src = bg;
+        context.drawImage(bgi, 0, 0, canvas.width, canvas.height);
+
+        context.font = '28px sans-serif';
+        context.fillStyle = '#ffffff';
+        context.fillText('Welcome', canvas.width / 2.5, canvas.height / 3.5);
+
+        context.font = applyText(canvas, `${cometta.user.username}!`);
+        context.fillStyle = '#ffffff';
+        context.fillText(`${cometta.user.username}}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+        context.beginPath();
+        context.arc(125, 125, 100, 0, Math.PI * 2, true);
+        context.closePath();
+        context.clip();
+
+        const { body } = await request(cometta.user.displayAvatarURL({ extension: 'jpg' }));
+        const avt = new Image();
+        avt.src = Buffer.from(await body.arrayBuffer());
+        context.drawImage(avt, 25, 25, 200, 200);
+
+        const img = new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'test.png' });
+
+        const welcomeMessage = new EmbedBuilder()
+            .setTitle(`Welcome ${cometta.user.username} to our Cometta Space Station`)
+            .setColor('#5e3fb4')
+            .setDescription('Remember to read our rule and answer the question in the server guide')
+            .setThumbnail(`https://cdn.discordapp.com/attachments/912573387560353836/1098189469410144376/Cometta-Logo-yChV3SYPL-transformed.png`)
+            .setImage('attachment://test.png')
+            .setTimestamp();
+
+            cometta.user.send({ embeds: [welcomeMessage], files: [img]});
     }
 }
