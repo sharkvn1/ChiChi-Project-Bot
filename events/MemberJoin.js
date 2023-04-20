4331218de40887415a372489a80591e01362e13f
Fumino <richberchannel01@gmail.com>
2023-04-20 07:39:37 +0700

    - Change text style in canvas

--- events/MemberJoin.js
+++ events/MemberJoin.js
@@ -7,7 +7,7 @@ const applyText = (canvas, text) => {
     const context = canvas.getContext('2d');
     let fontSize = 70;
     do {
-        context.font = `${fontSize -= 10}px tahoma bold italic`;
+        context.font = `bold italic ${fontSize -= 10}px tahoma`;
     } while (context.measureText(text).width > canvas.width - 300);
     return context.font;
 };
