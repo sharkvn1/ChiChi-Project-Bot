f48031d76a5d1ff8e1fe849f7512ec6617348173
Fumino Dev <richberchannel01@gmail.com>
2023-05-14 18:39:02 +0700

    +Change cmd file name

--- commands/adminCmd/voiceChannel.js
+++ commands/adminCmd/voiceChannel.js
@@ -1,36 +1,35 @@
 const { SlashCommandBuilder, ButtonBuilder, EmbedBuilder, ActionRowBuilder, PermissionFlagsBits, ChannelType, Interaction, ButtonStyle } = require('discord.js');
-const botConfig = require('../../database/models/botConfig')
+const guildConfig = require('../../database/guildConfig');
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('lobby')
-        .setDescription('Config your lobby')
+        .setName('voicechannel')
         .setNameLocalizations({
-            vi: 'lobby',
+            vi: 'kênh_thoại',
         })
+        .setDescription('Config Bot Voice Channel Function')
         .setDescriptionLocalizations({
-            vi: 'cấu hình lobby',
+            vi: 'cài đặt chức năng kênh thoại của bot',
         })
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .setDMPermission(false)
-
         .addSubcommand(subcommand =>
             subcommand
-                .setName('add')
-                .setDescription('add a new lobby')
+                .setName('lobby_add')
+                .setDescription('add a new temporary voice channel lobby')
                 .setNameLocalizations({
-                    vi: 'thêm'
+                    vi: 'thêm_sảnh'
                 })
                 .setDescriptionLocalizations({
-                    vi: 'thêm lobby mới'
+                    vi: 'thêm sảnh kênh thoại tạm thời mới'
                 })
                 .addChannelOption(option =>
                     option.setName('channel')
                         .setNameLocalizations({
                             vi: 'kênh',
                         })
-                        .setDescription('channel you want to be a new lobby')
+                        .setDescription('voice channel you want to be a new lobby')
                         .setDescriptionLocalizations({
-                            vi: 'kênh bạn muốn add làm lobby mới',
+                            vi: 'kênh thoại bạn muốn thêm làm sảnh mới',
                         })
                         .addChannelTypes(ChannelType.GuildVoice)
                         .setRequired(true)
@@ -38,23 +37,23 @@ module.exports = {
         )
         .addSubcommand(subcommand =>
             subcommand
-                .setName('remove')
-                .setDescription('romeve a lobby')
+                .setName('lobby_remove')
                 .setNameLocalizations({
-                    vi: 'xóa',
+                    vi: 'xóa_sảnh',
                 })
+                .setDescription('romeve a lobby')
                 .setDescriptionLocalizations({
-                    vi: 'xóa lobby'
+                    vi: 'xóa sảnh hiện có'
                 })
                 .addChannelOption(option =>
                     option
                         .setName('channel')
-                        .setDescription('channel you want to be removed')
+                        .setDescription('lobby you want to be removed')
                         .setNameLocalizations({
                             vi: 'kênh'
                         })
                         .setDescriptionLocalizations({
-                            vi: 'lobby bạn muốn xóa'
+                            vi: 'sảnh bạn muốn xóa'
                         })
                         .addChannelTypes(ChannelType.GuildVoice)
                         .setRequired(true)
@@ -62,13 +61,13 @@ module.exports = {
         )
         .addSubcommand(subcommand =>
             subcommand
-                .setName('setup')
-                .setDescription('setup voiceChannel dashboard')
+                .setName('dashboard')
+                .setDescription('setup temporary voiceChannel dashboard')
                 .setNameLocalizations({
-                    vi: 'thiết-lập'
+                    vi: 'bảng_điều_khiển'
                 })
                 .setDescriptionLocalizations({
-                    vi: 'Thiết lập bảng điều khiển'
+                    vi: 'Thiết lập bảng điều khiển kênh thpại tạm thời'
                 })
                 .addChannelOption(option =>
                     option
@@ -83,6 +82,17 @@ module.exports = {
                         .addChannelTypes(ChannelType.GuildText)
                         .setRequired(true)
                 )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('lobby_list')
+                .setDescription('list of lobby you have in server')
+                .setNameLocalizations({
+                    vi: 'danh_sách_sảnh'
+                })
+                .setDescriptionLocalizations({
+                    vi: 'danh sách những sảnh bạn có trong máy chủ'
+                })
         ),
     /**
      * 
@@ -92,32 +102,30 @@ module.exports = {
         const { options } = interaction;
 
         const cmd = options.getSubcommand();
-        const textChannel = options.getChannel('channel');
+        const Channel = options.getChannel('channel');
+        const guildId = interaction.guildId;
 
         switch (cmd) {
-            case 'add':
-                try {
-                    await botConfig.create({
-                        voiceChannelId: textChannel.id,
-                        guildId: interaction.member.guild.id,
-                    }).then(vc => {
-                        interaction.reply('Successfully added');
-                    })
-                } catch (err) {
-                    if (err === 'SequelizeUniqueConstraintError') {
-                        // TODO: finish the reply
-                        return interaction.reply('err')
-                    }
-                }
+            case 'lobby_add':
+                await guildConfig.updateOne(
+                    { guildId: guildId, },
+                    { $push: { voiceChannelId: Channel } }
+                ).then(vc => {
+                    interaction.reply(`Successfully added ${Channel}`);
+                })
+                break;
+            case 'lobby_remove':
+                await guildConfig.updateOne(
+                    { guildId: guildId, },
+                    { $pull: { voiceChannelId: Channel } }
+                ).then(data => {
+                    if (data.modifiedCount === 0) {
+                        return interaction.reply('cannot find channel');
+                    } else interaction.reply(`successfully removed **${textChannel.name}**`)
+                })
                 break;
 
-            case 'remove':
-                const find = await botConfig.destroy({ where: { voiceChannelId: textChannel.id } });
-                if (!find) return interaction.reply('cannot find channel');
-                interaction.reply(`successfully removed **${textChannel.name}**`)
-                break;
-
-            case 'setup':
+            case 'dashboard':
                 const dashboard = new EmbedBuilder()
                     .setColor('#5e3fb4')
                     .setAuthor({ name: 'VoiceChannel Dashboard', iconURL: interaction.user.displayAvatarURL() })
@@ -161,8 +169,20 @@ module.exports = {
                             .setLabel('Customize')
                             .setCustomId('customize'),
                     )
-                interaction.reply({ content: `Successfully setup voiceChannel dashboard in ${textChannel}`, ephemeral: true })
-                textChannel.send({ embeds: [dashboard], components: [rowOne, rowTwo] })
+                interaction.reply({ content: `Successfully setup voiceChannel dashboard in ${Channel}`, ephemeral: true })
+                Channel.send({ embeds: [dashboard], components: [rowOne, rowTwo] })
+                break;
+            case 'lobby_list':
+                const listEmbed = new EmbedBuilder()
+                    .setColor('#5e3fb4')
+                    .setAuthor({ name: `list of lobby in **${interaction.guild.name}**`, iconURL: interaction.guild.iconURL() })
+                    .setFooter({ text: interaction.client.user.username, iconURL: interaction.client.user.avatarURL() })
+                const listData = await guildConfig.findOne({ guildId: guildId })
+                for (const i in listData.voiceChannelId) {
+                    listEmbed
+                        .addFields({ name: `Lobby ${i + 1}`, value: `<#${listData.voiceChannelId[i]}>` })
+                }
+                interaction.reply({ embeds: [listEmbed], ephemeral: true });
                 break;
         }
     }
